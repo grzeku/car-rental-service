b5c3e7969aa56e9db7efce031f4abae3a4669939
grzegorzkuczera <kuczera.grzegorz@gmail.com>
2022-09-26 17:20:40 +0200

    first commit

---
+++ src/main/java/com/pl/carrentalservice/CarRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pl.carrentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalServiceApplication.class, args);
+	}
+
+}
