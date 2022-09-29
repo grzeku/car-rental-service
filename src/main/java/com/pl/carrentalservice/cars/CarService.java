d5b9724cf8862d180fec9a9c7eae77c79d105a65
Mateusz <Mateuszochal9310@gmail.com>
2022-09-29 21:19:01 +0200

    working hibernate/online DB

--- src/main/java/com/pl/carrentalservice/cars/CarService.java
+++ src/main/java/com/pl/carrentalservice/cars/CarService.java
@@ -1,24 +1,29 @@
 package com.pl.carrentalservice.cars;
 
+import com.pl.carrentalservice.util.HibernateUtil;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CarService {
+    final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    final EntityManager entityManager = sessionFactory.createEntityManager();
     private final CarRepository repository;
     public List<Car> getAll() { return repository.findAll();}
 
     public void addCars(List <Car> cars) { repository.saveAll(cars);}
 
     public void saveCars(List<Car> cars) {
-//        entityManager.getTransaction().begin();
+      // entityManager.getTransaction().begin();
         repository.saveAll(cars);
-//        entityManager.getTransaction().commit();
+        //entityManager.getTransaction().commit();
     }
 
 
