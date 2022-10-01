d0f56c5ade62ac0d1deae14b06aa80570e06d33a
Mateusz <Mateuszochal9310@gmail.com>
2022-10-01 13:53:44 +0200

    small changes

--- src/main/java/com/pl/carrentalservice/cars/CarRepository.java
+++ src/main/java/com/pl/carrentalservice/cars/CarRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
-public interface CarRepository extends JpaRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<Car, Integer> {
 }
--- src/main/java/com/pl/carrentalservice/employees/EmployeeRepository.java
+++ src/main/java/com/pl/carrentalservice/employees/EmployeeRepository.java
@@ -2,5 +2,5 @@ package com.pl.carrentalservice.employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
